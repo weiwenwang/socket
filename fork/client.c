e62108c71bf781d578772f55f185391ef1d69f62
wang <641257395@qq.com>
2018-01-25 19:07:03 +0800

    通过域名创建主动套接字

--- fork/client.c
+++ fork/client.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
 #define ERR_EXIT(m) \
     do \
@@ -14,14 +15,25 @@
 
 int main() {
     printf("welcome to thomas's socket \n");
+    char *host = "127.0.0.1";
+    // char *host = "wcc.push.com";  // host file: 127.0.0.1 wcc.push.com
+    int port = 1234;
     //创建套接字
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     //向服务器（特定的IP和端口）发起请求
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
+    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用'\0'填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
+
+    unsigned long inaddr = inet_addr(host);  //具体的IP地址或者域名
+    if (inaddr != INADDR_NONE) {
+        memcpy(&serv_addr.sin_addr, &inaddr, sizeof(inaddr));
+    } else {
+        struct hostent *hp = gethostbyname(host);
+        memcpy(&serv_addr.sin_addr, hp->h_addr, hp->h_length);
+    }
+
+    serv_addr.sin_port = htons(port);  //端口
     if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         ERR_EXIT("connect");
     }
